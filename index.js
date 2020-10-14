54753df6be858bbbf97a6fdf22945cf0ee92b9da
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2020-10-14 16:26:02 -0700

    comleted all tasks

--- index.js
+++ index.js
@@ -39,10 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
-
+Person.prototype.eat = function(food) {
+    if(this.stomach.length < 10){
+      this.stomach.push(food);
+    }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name},${this.age}`;
+}
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,10 +69,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,miles) {
+this.model = model;
+this.milesPerGallon = miles;
+this.tank = 0;
+this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+Car.prototype.drive = function(distance){
+  let drivableMiles = this.tank * this.milesPerGallon;
+  if(distance >= drivableMiles){
+    this.odometer += drivableMiles;
+    this.tank -= drivableMiles/this.milesPerGallon;
+  return `I ran out of fuel at ${this.odometer} miles!`;  
+}
+else {
+  this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
+}
+console.log(this.tank);
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -68,16 +98,21 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favToy) {
+Person.call(this, name, age);
+this.favoriteToy = favToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
+  1. this keyword refers to the object which declared just before it. if there is no object declared before this keyword it refers to the global window object
+  2. in imlicit binding this refers to the that object which is to the left from the dot
   3. 
   4. 
 */
